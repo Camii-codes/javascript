45b8de1c644d6fdfee4bca7847ca5e2a39a0ba39
Camila Andrade <camilaluanarodrigues9@gmail.com>
2023-11-20 16:15:12 -0300

    adicionei uma nova condição

---
+++ ex009/composta.js
@@ -0,0 +1,7 @@
+let pais = 'Brasil';
+console.log(`Vivendo em ${pais}`);
+if (pais == 'Brasil') { //Condiçaão composta
+    console.log('Você é Brasileiro!');
+} else {
+    console.log('Você é Estrangeiro!');
+}
