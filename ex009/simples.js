f25d2f50d9d57d2b3a23e696e6ddf8466de6153b
Camila Andrade <camilaluanarodrigues9@gmail.com>
2023-11-20 15:12:01 -0300

    Comecei estudar condições

---
+++ ex009/simples.js
@@ -0,0 +1,6 @@
+let vel = 70;
+console.log(`A velocidade do seu carro é de R${vel}Km/h`);
+if (vel > 60) { //Condição simples
+    console.log('Você ultrapassou a velocidade permitida. MULTADO!');
+}
+console.log('Dirija sempre usando o cinto de segurança!');
