aa0bd05305bb01cfd1cdf3a3bc7fdfa33022ae40
Camila Andrade <camilaluanarodrigues9@gmail.com>
2023-11-26 12:30:24 -0300

    Múltipla

---
+++ ex010/multipla.js
@@ -0,0 +1,29 @@
+let agora = new Date();
+let diaSem = agora.getDay();
+
+switch (diaSem) {
+    case 0:
+        console.log('Hoje é: Domingo');
+        break
+    case 1:
+        console.log('Hoje é: Segunda');
+        break
+    case 2: 
+        console.log('Hoje é: Terça');
+        break
+    case 3: 
+        console.log('Hoje é: Quarta');
+        break
+    case 4: 
+        console.log('Hoje é: Quinta');
+        break
+    case 5: 
+        console.log('Hoje é: Sexta');
+        break
+    case 6: 
+        console.log('Hoje é: Sábado');
+        break
+    default:
+        console.log('[ERRO] Data inválida.');
+        break
+    }
