28ec4ea56f00bba0b4ec2b38670c792e894746eb
Camila Andrade <camilaluanarodrigues9@gmail.com>
2023-11-29 14:56:23 -0300

    update

--- ex011/ex013/script.js
+++ ex011/ex013/script.js
@@ -1,4 +1,38 @@
 function verificar() {
     let data = new Date ();
     let ano = data.getFullYear();
+    let fano = window.document.getElementById('txtano');
+    let res = window.document.querySelector('div#res');
+
+    if (fano.value.length == 0 || Number(fano.value) > ano) {
+        window.alert('[ERRO] Verifique os dados e tente novamente.');
+    } else {
+        let fsex = window.document.getElementsByName('radsex');
+        let idade = ano - Number(fano.value);
+        let genero = '';
+        let img = document.createElement('img');
+        img.setAttribute('id', 'img');
+        
+        if (fsex[0].checked) {
+            genero = 'Homem';
+            if (idade >= 0 && idade < 10) {
+                // Criança
+                img.setAttribute('src', 'imagens/foto-crianca-m.png');
+            } else if (idade < 21) {
+                // Jovem
+                img.setAttribute('src', 'imagens/foto-jovem-m.png');
+            } else if (idade < 50) {
+                // Adulto
+                img.setAttribute('src', 'imagens/foto-adulto-m.png');
+            } else {
+                // Idoso
+                img.setAttribute('src', 'imagens/foto-idoso-m.png');
+            }
+        } else if (fsex[1].checked) {
+            genero = 'Mulher';
+        }
+        res.style.textAlign = 'center';
+        res.innerHTML = `Detectamos ${genero} com ${idade} anos`; 
+        res.appendChild(img);
+    }
 }
