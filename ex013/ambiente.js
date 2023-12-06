ac87623f122f7dcb89ad9906c20aee34a8240972
Camila Andrade <camilaluanarodrigues9@gmail.com>
2023-12-06 10:45:15 -0300

    update

--- ex013/ambiente.js
+++ ex013/ambiente.js
@@ -1,13 +1,5 @@
-/* let c = 1;
-
-while (c <= 5) {
-    console.log(c)
-    c++
-}
-*/
-
-// ------------------------
-
+console.log('Vai começar...');
 for (let c = 1; c <= 5; c++) {
     console.log(c)
 }
+console.log('FIM!');
