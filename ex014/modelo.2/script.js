c080a8f65aac801c1e528a5ba6489be288d6242f
Camila Andrade <camilaluanarodrigues9@gmail.com>
2023-12-07 10:49:34 -0300

    for

--- ex014/modelo.2/script.js
+++ ex014/modelo.2/script.js
@@ -1,4 +1,5 @@
-function contar() {
+function contar(event) {
+    event.preventDefault();
     let ini = window.document.getElementById('numi');
     let fim = window.document.getElementById('numf');
     let pass = window.document.getElementById('nump');
@@ -7,6 +8,16 @@ function contar() {
     if (ini.value.length == 0 || fim.value.length == 0 || pass.value.length == 0) {
         window.alert('[ERRO] Faltam dados!');
     } else {
-        window.alert('TUDO OK!');
+        res.innerHTML = 'Contando: '
+        let i = Number(ini.value);
+        let f = Number(fim.value);
+        let p = Number(pass.value);
+
+        /* O contador vai começar no ÍNICIO e enquanto
+         o contador for menor ou igual ao FIM, o contador
+          vai receber ele mesmo mais o PASSO */
+        for(let c = i; c <= f; c += p) {
+            res.innerHTML += ` ${c} \u{1F449}` 
+        }
     }
-}
+} 
