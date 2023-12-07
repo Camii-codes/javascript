51f2d787ca4d09ee5b1a03e9c080c0ae6604847e
Camila Andrade <camilaluanarodrigues9@gmail.com>
2023-12-07 09:08:23 -0300

    update

--- ex014/modelo.2/script.js
+++ ex014/modelo.2/script.js
@@ -1,1 +1,12 @@
-alert('Olá!');
+function contar() {
+    let ini = window.document.getElementById('numi');
+    let fim = window.document.getElementById('numf');
+    let pass = window.document.getElementById('nump');
+    let res = window.document.getElementById('res');
+
+    if (ini.value.length == 0 || fim.value.length == 0 || pass.value.length == 0) {
+        window.alert('[ERRO] Faltam dados!');
+    } else {
+        window.alert('TUDO OK!');
+    }
+}
