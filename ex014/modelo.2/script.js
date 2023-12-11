2c012487f06f1a184fd9a30a29ea760186b63af5
Camila Andrade <camilaluanarodrigues9@gmail.com>
2023-12-11 11:01:44 -0300

    exercício finalizado

--- ex014/modelo.2/script.js
+++ ex014/modelo.2/script.js
@@ -7,8 +7,9 @@ function contar(event) {
 
     if (ini.value.length == 0 || fim.value.length == 0 || pass.value.length == 0) {
         window.alert('[ERRO] Faltam dados!');
+        res.innerHTML =  'Imposível contar!'
     } else {
-        res.innerHTML = 'Contando: '
+        res.innerHTML = 'Contando: <br>'
         let i = Number(ini.value);
         let f = Number(fim.value);
         let p = Number(pass.value);
@@ -16,8 +17,22 @@ function contar(event) {
         /* O contador vai começar no ÍNICIO e enquanto
          o contador for menor ou igual ao FIM, o contador
           vai receber ele mesmo mais o PASSO */
-        for(let c = i; c <= f; c += p) {
-            res.innerHTML += ` ${c} \u{1F449}` 
+        if (p <= 0) {
+            window.alert('Passo inválido! Considerando PASSO 1');
+            p = 1;
         }
+        
+          if (i < f) {
+            // Contagem crescente
+            for(let c = i; c <= f; c += p) {
+                res.innerHTML += ` ${c} \u{1F449}` 
+            }
+        } else {
+            // Contagem regressiva
+            for(let c = i; c >= f; c -= p) {
+                res.innerHTML += ` ${c} \u{1F449}` 
+            }
+        }
+        res.innerHTML += `\u{1F3C1}`;
     }
 } 
