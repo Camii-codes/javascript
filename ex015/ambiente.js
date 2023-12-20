853f24f1e0adb9e67acb514396c2e2a7760a925f
Camila Andrade <camilaluanarodrigues9@gmail.com>
2023-12-20 12:35:11 -0300

    Arrays

---
+++ ex015/ambiente.js
@@ -0,0 +1,7 @@
+let num = [5, 8, 2, 9, 3]
+
+num.sort() // A ordem influência
+num.push(1)
+console.log(num)
+console.log(`O vetor tem ${num.length} posições`)
+console.log(`O primeiro valor do vetor é ${num[4]}`)
